a8891f2017b534d528daf3181b1982857195a585
Rajesh Kumar <rajeshkumar120402@gamil.com>
2025-08-29 02:02:05 +0530

    user and video schema

---
+++ src/models/users.model.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // url cloudinary
+      required: true,
+    },
+    coverimage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+userSchema.pre("save" , async function (next){
+    if(!this.password.isModified()) return next() ;
+    this.password = bcrypt.hash(this.password , 10);
+    next();
+})
+export const User = mongoose.model("User", userSchema);
