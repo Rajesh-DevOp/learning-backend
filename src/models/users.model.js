3514d6b0e02824cdc8c33b8142f13d2df202fab7
Rajesh Kumar <rajeshkumar120402@gamil.com>
2025-10-01 00:09:58 +0530

    usermodel and video model

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -56,4 +56,33 @@ userSchema.pre("save" , async function (next){
     this.password = bcrypt.hash(this.password , 10);
     next();
 })
+
+userSchema.methods.isPasswordCorrect = async function (password){
+  return await bcrypt.compare(password ,this.password );
+}
+
+userSchema.methods.generateAccessToke = function (){
+  return jwt.sign(
+    {
+      _id :this._id,
+      email :this.email,
+      username:this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn : process.env.ACCESS_TOKEN_EXPIRE
+    }
+  )
+}
+userSchema.methods.generateRefreshToken = function (){
+  return jwt.sign(
+    {
+      _id :this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn : process.env.REFRESH_TOKEN_EXPIRE
+    }
+  )
+}
 export const User = mongoose.model("User", userSchema);
